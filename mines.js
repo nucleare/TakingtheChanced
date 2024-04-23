83d7eecb15edb7fc7658daa8da6a208e41f5f3a5
nucleare <29671486+nucleare@users.noreply.github.com>
2024-04-23 07:43:56 -0800

    Update mines.js
    Added decrease bet by 1/2 and increase bet by 2x hotkeys

--- mines.js
+++ mines.js
@@ -1,10 +1,26 @@
 document.addEventListener('keydown', function(event) {
-    if (event.code === 'Space' && !['INPUT', 'TEXTAREA'].includes(document.activeElement.tagName)) {
-        event.preventDefault();
-      // Spacebar activates Play button
-        const playButton = Array.from(document.querySelectorAll("button.btn-bet")).find(button => button.textContent.includes('Play'));
-        if (playButton) {
-            playButton.click();
+    // Check if the active element is not an input or textarea
+    if (!['INPUT', 'TEXTAREA'].includes(document.activeElement.tagName)) {
+        let button;
+        
+        switch (event.key) {
+            case ' ':
+                // Spacebar
+                button = Array.from(document.querySelectorAll("button.btn-bet")).find(button => button.textContent.includes('Play'));
+                break;
+            case 'a':
+                // 'a' key for "1/2" button
+                button = Array.from(document.querySelectorAll("button.btn-multiplier")).find(button => button.textContent.includes('1/2'));
+                break;
+            case 's':
+                // 's' key for "2x" button
+                button = Array.from(document.querySelectorAll("button.btn-multiplier")).find(button => button.textContent.includes('2x'));
+                break;
+        }
+
+        if (button) {
+            event.preventDefault();  // Prevent default action if a button is found and will be clicked
+            button.click();
         }
     }
 });
